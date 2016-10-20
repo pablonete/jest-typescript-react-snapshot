ba69e76749cebb0de3e9e6cf2a3f35092c23b022
Pablo Núñez <pablonete@gmail.com>
2016-10-20 18:15:30 +0200

    New MultiplicationLine component.

--- MultiplicationTable.tsx
+++ MultiplicationTable.tsx
@@ -7,9 +7,14 @@ export interface Props {
 export const MultiplicationTable = (props: Props): JSX.Element =>
   <ul>
     { getRange(1, 10).map(i => 
-      <li key={i}>{props.factor} x {i} = {props.factor*i}</li>
+      <MultiplicationLine key={i} multiplicand={i} {...props} />
     )}
   </ul>;
+
+const MultiplicationLine = (props: { multiplicand: number, factor: number }): JSX.Element =>
+  <li>
+    {props.factor} x {props.multiplicand} = {props.factor * props.multiplicand}
+  </li>;
 
 function getRange(from: number, to: number): number[] {
   return Array.apply(null, Array(to - from + 1)).map((_, i) => i + from);
